6e951d7b5abd1b0e07b47bc364673314b04fc204
Nicolaas <github@sunnysideup.co.nz>
2020-09-10 08:29:07 +1200

    Update includes/functions.php
    Co-authored-by: Daniel Hensby <dhensby@users.noreply.github.com>

--- includes/functions.php
+++ includes/functions.php
@@ -31,7 +31,7 @@ if (!function_exists('SilverStripe\\StaticPublishQueue\\URLtoPath')) {
         $filename = $urlSegment ?: 'index';
 
         if ($domainBasedCaching) {
-            if (! $urlParts) {
+            if (!$urlParts) {
                 throw new \LogicException('Unable to parse URL');
             }
             if (isset($urlParts['host'])) {
